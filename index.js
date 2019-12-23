24db24de56b5a5087f1885c65e142ce998fa5699
Jonas Friedmann <j@frd.mn>
2019-12-23 16:02:50 +0100

    Process YAML content

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 const {	Rocketchat } = require('@rocket.chat/sdk');
 const program = require('commander');
+const request = require('request-promise');
+const YAML = require('yaml')
+const path = require('path')
 
 // Construct args parser
 program
-    .option('-f, --file <path>', 'Path of the emoji file')
-    .option('-n, --emojiname <emoji>', 'Name of the emoji')
-    .option('-a, --aliasname <name>', 'Optional alias of the emoi')
     .option('-y, --yaml <url>', 'URL to Emojipacks YAML file')
     .parse(process.argv);
-
 
 // Show help if no option is given
 if (!process.argv.slice(2).length) {
@@ -37,75 +36,60 @@ const logger = false || {
 	error: (...args) => { console.error(args)},
 };
 
-// Function to parse args of script
-async function parseArgs(){
-    var emojis = [];
-
-    // Check for --yaml switch
-    if (program.yaml) {
-        console.log('parse yaml file.')
-    // Check for single --file
-    } else if(program.file) {
-        // Make sure emoji name is set
-        if(!program.emojiname){
-            program.outputHelp();
-            process.exit(1);
-        }
-
-        // Construct object to process for upload
-        var emojiObject = {
-            emoji: true,
-            name: program.emojiname
-        };
-
-        // Check for optional emoji alias
-        if(program.aliasname){
-            emojiObject.alias = program.aliasname;
-        }
-
-        // Push into emojiObject
-        emojis.push(emojiObject);
-    }
-
-    console.log(emojis)
-}
-
 // Function to connect to RocketChat instance using SDK
 async function connect() {
     const client = new Rocketchat({
-        logger:console,
+        logger,
         host: config.hostname,
         useSsl: config.hostname.match("^https") ? true : false
     });
 
+    console.log('Logging in "' + config.username + '@'+ config.hostname + '"...');
     await client.connect();
     await client.login(credentials);
 
-    await uploadEmoji(client);
+    if (program.yaml) {
+        var emojipack = YAML.parse(await request(program.yaml));
+
+        for(emoji of emojipack.emojis){
+            // Construct object to process for upload
+            var emojiObject = {
+                src: emoji.src,
+                emoji: await request({uri: emoji.src, encoding: null}),
+                name: emoji.name
+            };
+
+            await uploadEmoji(client, emojiObject);
+        }
+    }
 }
 
-// Function to list emojis
-async function listEmoji(client) {
-    const emojis = await client.get('emoji-custom.list');
-    console.log(emojis.emojis.update.length + ' emojis found');
-}
+// // Function to list emojis
+// async function listEmoji(client) {
+//     const emojis = await client.get('emoji-custom.list');
+//     console.log(emojis.emojis.update.length + ' emojis found');
+// }
 
 // Function to upload a custom emoji
-async function uploadEmoji(client) {
+async function uploadEmoji(client, emojiObject) {
+    console.log('Trying to upload emoji "' + emojiObject.name + '"...');
     const form = new FormData();
-    form.append('emoji', 'test');
-    form.append('name', 'test');
-    form.append('aliases', 'test');
+    form.append('emoji', emojiObject.emoji, 'emoji.' + path.extname(emojiObject.src));
+    form.append('name', emojiObject.name);
+
     client.getBody = (data) => data;
-    const emojis = await client.post("emoji-custom.create", form, true, new RegExp(''), {
+    const response = await client.post("emoji-custom.create", form, true, new RegExp(''), {
         customHeaders: {
             ...client.client._headers,
         }
     })
-    console.log(emojis);
+
+    // Check for API response
+    if(response.success){
+        console.log('=> Successfully uploaded "' + emojiObject.name + '"!');
+    } else {
+        console.log('=> Couldn\'t upload "' + emojiObject.name + '": ' + response.error);
+    }
 }
 
-parseArgs();
-
-// Execute connect function
-// connect();
+connect();
